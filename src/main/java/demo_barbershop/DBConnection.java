663c4ef6ab23afe1920f163b39b297a7627aa071
Irsya Shah <irsyashah11@gmail.com>
2026-02-17 22:25:30 +0800

    Update DB connection

--- src/main/java/demo_barbershop/DBConnection.java
+++ src/main/java/demo_barbershop/DBConnection.java
@@ -4,7 +4,7 @@ import java.sql.*;
 
 public class DBConnection {
 
-	private static final String URL = "jdbc:mysql://localhost:3306/demo_sbarbershop";
+	private static final String URL = "jdbc:mysql://localhost:3306/demo_barbershop";
 	private static final String USER = "root";
 	private static final String PASS = "";
 
